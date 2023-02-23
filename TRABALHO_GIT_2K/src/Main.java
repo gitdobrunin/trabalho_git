258630ef48cc3650699df28753ecbe9b57d710f8
Luiz Kovalick <luizkovalick.ss034@academico.ifsul.edu.br>
2023-02-23 08:39:56 -0300

    Adicionado funcionalidade digitar o nome e imprimir na tela

---
+++ TRABALHO_GIT_2K/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner t = new Scanner(System.in);
+        System.out.println("Bruno Roso Ribas");
+        String a = t.nextLine();
+        System.out.println("Olá" + a);
+        System.out.println("Olá mundo");
+        
+        
+        
+	}
+
+}
